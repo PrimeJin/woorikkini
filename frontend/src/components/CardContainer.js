9e06ba253fa212a728abb146e93aebe37f53a94e
SJJ <jeenju1995@gmail.com>
2023-01-30 17:53:53 +0900

    [S08P12A804-119] FE/Feat: create memory-card component

--- frontend/src/components/CardContainer.js
+++ frontend/src/components/CardContainer.js
@@ -6,9 +6,9 @@ import CreateCard from './CreateCard';
 
 // import { useState, useRef, useEffect } from "react";
 
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
 
 const items = [
   {
@@ -24,94 +24,56 @@ const items = [
 const SwiperContainer = () => {
   SwipeCore.use([Navigation, Pagination, Autoplay]);
 
-  // const Card = ({ title, src }) => {
-  //   return (
-  //   );
-  // };
-  // const prevRef = useRef(null);
-  // const nextRef = useRef(null);
-
-  // const [swiperSetting, setSwiperSetting] = useState(null);
-
-  // useEffect(() => {
-  //   if (!swiperSetting) {
-  //     const settings = {
-  //       // autoplay: { delay: 1000 },
-  //       navigation: {
-  //         prevEl: prevRef.current, // 이전 버튼
-  //         nextEl: nextRef.current // 다음 버튼
-  //       },
-  //       onBeforeInit: (swiper) => {
-  //         // 초기 설정
-  //         swiper.params.navigation.prevEl = prevRef.current;
-  //         swiper.params.navigation.nextEl = nextRef.current;
-  //         swiper.navigation.update();
-  //       },
-  //       pagination: {
-  //         el: ".swiper-pagination",
-  //         clickable: true,
-  //         dynamicBullets: true
-  //       }
-  //     };
-
-  //     setSwiperSetting(settings);
-  //   }
-  // }, [prevRef, nextRef, swiperSetting]);
-
   return (
     <>
       {/* {swiperSetting && ( */}
       <div className="cardlist-top">
-        <Swiper
-          pagination={{
-            clickable: true,
-            dynamicBullets: true,
-          }}
-          // navigation={{
-          //   nextEl: ".swiper-button-next",
-          //   prevEl: ".swiper-button-prev"
-          // }}
-          navigation
-          slidesPerView={1}
-          spaceBetween={20}
-          effect={'fade'}
-          loop={true}
-          speed={300}
-          className="swiper-container"
-        >
-          <div className="swiper-wrapper">
-            {items.map((item, idx) => {
-              return (
-                <SwiperSlide key={idx}>
-                  <div className="card">
-                    <div className="slider-image-wrapper" style={{ width: 291, height: 300, position: 'relative' }}>
+        <div className="card-whole">
+          <Swiper
+            pagination={{
+              clickable: true,
+              dynamicBullets: true,
+            }}
+            navigation
+            slidesPerView={1}
+            spaceBetween={20}
+            effect={'fade'}
+            loop={true}
+            speed={300}
+            className="swiper-container"
+          >
+            <div className="swiper-wrapper">
+              <div className="card swiper-slide">
+                {items.map((item, idx) => {
+                  return (
+                    <SwiperSlide key={idx}>
+                      {/* <div className="slider-image-wrapper"> */}
                       {/* <Card title={item.title} src={item.src} /> */}
                       {/* 이미지 들어갈 자리 */}
-                      <img className="slider-image" src={item.src} alt="SliderImg" />
-                    </div>
-                    {/* <div style={{ position: 'absolute' }}> */}
-                    <button>수정</button>
-                    <button>삭제</button>
-                    {/* </div> */}
-                    <div className="slider-item-content">
-                      <div>날짜</div>
-                      <h3>{item.title}</h3>
-                      <div>내용을 입력하세요.</div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-            <div className="slider-buttons">
-              <button className="swiper-button-prev">Prev</button>
-              <button className="swiper-button-next">Next</button>
+                      <img className="slider-image-wrapper" src={item.src} alt="SliderImg" />
+                      {/* </div> */}
+                    </SwiperSlide>
+                  );
+                })}
+                ;
+              </div>
+
+              <div className="slider-buttons">
+                <button className="swiper-button-prev">Prev</button>
+                <button className="swiper-button-next">Next</button>
+              </div>
             </div>
-            {/* <div className="swiper-pagination" /> */}
+          </Swiper>
+          <div>
+            <button>수정</button>
+            <button>삭제</button>
+            <div>날짜</div>
+            <div>제목을 입력하세요.</div>
+            <div>내용을 입력하세요.</div>
           </div>
-        </Swiper>
+        </div>
         <CreateCard></CreateCard>
       </div>
-      {/* )} */}
     </>
   );
 };
