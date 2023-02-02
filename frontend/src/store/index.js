2511afa8afecc8a4bc6abc6f24aed87d22901f34
한상원 <wkas1921@naver.com>
2023-02-02 10:59:53 +0900

    [S08P12A804-131] FE/Feat: Add user store file
    Users 상태관리를 위한 store 추가
    components 리팩토링

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,13 @@
  * /store로 연결하면 자동으로 여기로 연결되서 모든 reducer 모듈을 관리함
  */
 import { configureStore } from '@reduxjs/toolkit';
-import tokenReducer from './Auth';
+import { tokenSlice } from './Auth';
+import { userSlice } from './User';
 
 export default configureStore({
   reducer: {
-    token: tokenReducer,
+    token: tokenSlice.reducer,
+    user: userSlice.reducer,
   },
 
   devTools: process.env.NODE_ENV !== 'production',
