916bcef2c8e83140260435459e01882d66458341
nyh365 <12nyk34@naver.com>
2023-02-02 16:02:01 +0900

    [S08P12A804-192] BE/Feat: Add find Exit User function

---
+++ backend/src/main/java/com/ssafy/kkini/service/ExitService.java
@@ -0,0 +1,37 @@
+package com.ssafy.kkini.service;
+
+
+import com.ssafy.kkini.entity.Exit;
+import com.ssafy.kkini.entity.Room;
+import com.ssafy.kkini.repository.ExitRepository;
+import com.ssafy.kkini.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExitService {
+    private final RoomRepository roomRepository;
+    private final ExitRepository exitRepository;
+    public int addExitUser(String roomId, String userId) {
+        System.out.println("hihi");
+        Room room = roomRepository.findByRoomId(Integer.parseInt(roomId));
+        System.out.println(room);
+
+        if (room == null) {
+            return 0;
+        }
+        Exit exit = new Exit(Integer.parseInt(userId), room);
+        exitRepository.save(exit);
+        return 1;
+    }
+
+    public int findExitUser(String roomId, String userId) {
+        Exit exit = exitRepository.findByOutcaster(Integer.parseInt(userId));
+        if (exit == null || exit.getRoomId().getRoomId() != Integer.parseInt(roomId)){
+            return 0;
+        }
+
+        return 1;
+    }
+}
