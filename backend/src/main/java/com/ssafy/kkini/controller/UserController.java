fad5c76bbe1b2e6492b1ef12ae8b1afc034e3701
조우진 <sky5962146@gmail.com>
2023-02-02 16:53:49 +0900

    [S08P12A804-48] BE/Feat: Add RestController annotation
    -@RestController로 변경
    -@RequestMapping 추가
    -@CrossOrigin 추가

--- backend/src/main/java/com/ssafy/kkini/controller/UserController.java
+++ backend/src/main/java/com/ssafy/kkini/controller/UserController.java
@@ -8,15 +8,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
 public class UserController {
     private UserService userService;
     private AuthCodeService authCodeService;
