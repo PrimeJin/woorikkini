149d42677190f275e5f84f2d01832ef95f6ef952
YJW <ykm9397@naver.com>
2023-02-03 16:01:59 +0900

    [S08P12A804-150] FE/Feat: Add delete modal
    삭제시 모달 생성, CSS 적용

--- frontend/src/components/admin/Notice.js
+++ frontend/src/components/admin/Notice.js
@@ -1,27 +1,26 @@
-import NoticeList from './NoticeList';
-import './Notice.css';
-import axios from 'axios';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Pagination } from '@mui/material';
+import NoticeList from "./NoticeList";
+import "./Notice.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Pagination } from "@mui/material";
 
 function Notice() {
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
-  const [count, setCount] = useState('');
+  const [count, setCount] = useState("");
 
   function getList() {
     axios({
       url: `http://i8a804.p.ssafy.io:8050/notice/?page=${page - 1}`,
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => {
         setList(res.data.noticeList.content);
         setCount(res.data.noticeList.totalPages);
       })
       .catch((err) => {
-        console.log(err, '공지사항 에러');
+        console.log(err, "공지사항 에러");
       });
   }
 
@@ -31,7 +30,7 @@ function Notice() {
 
   const navigate = useNavigate();
   function save() {
-    navigate('create');
+    navigate("create");
   }
 
   function pageChange(e, page) {
@@ -40,12 +39,12 @@ function Notice() {
 
   return (
     <div>
-      <h1 style={{ marginTop: '2%', marginBottom: 0 }}>공지사항</h1>
+      <h1 style={{ margin: "1%", marginTop: "3%" }}>공지사항</h1>
       <div
         style={{
-          justifyContent: 'right',
-          display: 'flex',
-          marginRight: '5%',
+          justifyContent: "right",
+          display: "flex",
+          marginRight: "5%",
           marginBottom: 0,
         }}
       >
@@ -56,17 +55,17 @@ function Notice() {
 
       <div
         style={{
-          justifyContent: 'center',
-          display: 'flex',
-          margin: '1%',
-          flexFlow: 'wrap',
+          justifyContent: "center",
+          display: "flex",
+          margin: "1%",
+          flexFlow: "wrap",
         }}
       >
         <table>
           <tr>
-            <th style={{ width: '10%' }}>번호</th>
-            <th style={{ width: '70%' }}>제목</th>
-            <th style={{ width: '20%' }}>작성 날짜</th>
+            <th style={{ width: "10%" }}>번호</th>
+            <th style={{ width: "70%" }}>제목</th>
+            <th style={{ width: "20%" }}>작성일자</th>
           </tr>
           {list.map((notice, index) => (
             <NoticeList key={index} notice={notice} />
--- frontend/src/components/admin/NoticeDetail.js
+++ frontend/src/components/admin/NoticeDetail.js
@@ -1,79 +1,92 @@
-import axios from 'axios';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./Notice.css";
 
 const NoticeDetail = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [time, setTime] = useState("");
   const params = useParams();
-  const [update, setUpdate] = useState('');
+  const [update, setUpdate] = useState("");
+  const [modal, setModal] = useState(false);
 
   function getDetail() {
     axios({
       url: `http://i8a804.p.ssafy.io:8050/notice/${params.noticeId}`,
-      methods: 'GET',
+      methods: "GET",
     })
       .then((res) => {
         setTitle(res.data.notice.noticeTitle);
         setContent(res.data.notice.noticeContent);
+        setTime(res.data.notice.createdTime.substr(0, 10));
       })
       .catch((err) => {
-        console.log(err, '공지사항 디테일 에러');
+        console.log(err, "공지사항 디테일 에러");
       });
   }
 
   useEffect(() => {
     getDetail();
-  }, []);
+    console.log(content);
+  }, [update]);
 
-  const [inputTitle, setInputTitle] = useState('');
-  const [inputContent, setInputContent] = useState('');
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
 
   useEffect(() => {
     setInputTitle(title);
     setInputContent(content);
-  }, [title, content]);
+  }, [title, content, update]);
 
   const navigate = useNavigate();
 
   function list() {
-    navigate('/admin/notice');
+    navigate("/admin/notice");
   }
 
   function goToUpdate() {
-    setUpdate('1');
+    setUpdate("1");
   }
 
   function back() {
-    setUpdate('');
+    setUpdate("");
   }
 
   function noticeDelete() {
     axios({
-      method: 'DELETE',
+      method: "DELETE",
       url: `http://i8a804.p.ssafy.io:8050/notice/${params.noticeId}/`,
       data: {
         noticeId: params.noticeId,
       },
     })
       .then((res) => {
-        alert('삭제가 완료되었습니다.');
-        navigate('/admin/notice');
+        alert("삭제가 완료되었습니다.");
+        navigate("/admin/notice");
       })
       .catch((err) => {
-        console.log('notice 삭제 에러');
+        console.log("notice 삭제 에러");
       });
   }
 
+  function DeleteModal() {
+    setModal(true);
+  }
+  function modalClose() {
+    if (modal === true) {
+      setModal(false);
+    }
+  }
+
   function updateConfirm() {
-    if (inputTitle.trim() === '') {
-      alert('제목을 입력해주세요');
-    } else if (inputContent.trim() === '') {
-      alert('내용을 입력해주세요');
+    if (inputTitle.trim() === "") {
+      alert("제목을 입력해주세요");
+    } else if (inputContent.trim() === "") {
+      alert("내용을 입력해주세요");
     } else {
       axios({
-        method: 'put',
+        method: "put",
         url: `http://i8a804.p.ssafy.io:8050/notice/${params.noticeId}`,
         data: {
           noticeId: params.noticeId,
@@ -82,77 +95,76 @@ const NoticeDetail = () => {
         },
       })
         .then((res) => {
-          alert('수정이 완료되었습니다.');
-          window.location.replace(`${params.noticeId}`);
+          alert("수정이 완료되었습니다.");
+          setUpdate("");
         })
         .catch((err) => {
-          console.log('공지사항 수정 에러', err);
+          console.log("공지사항 수정 에러", err);
         });
     }
   }
 
-  //   if (!update) {
-  //     return (
-  //       <div style={{ display: "flex", flexWrap: "wrap" }}>
-  //         <h1>공지사항</h1>
-  //         <div className="detail">
-  //           제목: {title}
-  //           <br />
-  //           내용: {content}
-  //           <br />
-  //           <button onClick={goToUpdate}>수정</button>
-  //           <button onClick={noticeDelete}>삭제</button>
-  //         </div>
-  //         <button onClick={list}>목록으로</button>
-  //       </div>
-  //     );
-  //   } else {
-  //     return (
-  //       <div>
-  //         <h1>공지사항</h1>
-  //         <div>
-  //           <label>제목</label>
-  //           <br />
-  //           <input
-  //             type="text"
-  //             value={inputTitle}
-  //             onChange={(e) => setInputTitle(e.target.value)}
-  //           />
-  //           <br />
-  //           <label>내용</label>
-  //           <br />
-  //           <input
-  //             type="text"
-  //             value={inputContent}
-  //             onChange={(e) => setInputContent(e.target.value)}
-  //           />
-  //           <br />
-  //           <button onClick={updateConfirm}>확인</button>
-  //           <button onClick={back}>취소</button>
-  //         </div>
-  //         <button onClick={list}>목록으로</button>
-  //       </div>
-  //     );
-  //   }
-  // };
-
   return (
     <div>
       <h1>공지사항</h1>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+
+      {/* 모달 시작 */}
+      {modal ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            position: "fixed",
+            backgroundColor: "rgba(0,0,0,0)",
+            top: "30%",
+          }}
+        >
+          <div className="delete">
+            <p style={{ width: "100%", marginBottom: 0 }}>
+              해당 글을 삭제하시겠습니까?
+            </p>
+            <button className="modalBtn" onClick={noticeDelete}>
+              확인
+            </button>
+            <button className="modalBtn" onClick={modalClose}>
+              취소
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {/* 모달 끝 */}
+
+      <div
+        style={{ display: "flex", justifyContent: "center" }}
+        onClick={modalClose}
+      >
         {!update ? (
+          //
+          // 디테일
           <div className="detail">
             <p className="title">{title}</p>
+            <div style={{ marginLeft: "80%", marginBottom: "2%" }}>
+              <small>작성일자: {time}</small>
+            </div>
             <hr />
-            <p className="content">{content}</p>
+            <pre className="content">{content}</pre>
             <button className="btn" onClick={goToUpdate}>
               수정
             </button>
-            <button className="btn" onClick={noticeDelete} style={{ backgroundColor: '#FF8D89' }}>
+            <button
+              className="btn"
+              onClick={DeleteModal}
+              style={{ backgroundColor: "#FF8D89" }}
+            >
               삭제
             </button>
           </div>
         ) : (
+          //
+          // 수정
           <div className="update">
             <label>제목</label>
             <br />
@@ -165,7 +177,7 @@ const NoticeDetail = () => {
             <br />
             <label>내용</label>
             <br />
-            <input
+            <textarea
               className="inputContent"
               type="text"
               value={inputContent}
@@ -181,7 +193,9 @@ const NoticeDetail = () => {
           </div>
         )}
       </div>
-      <div style={{ justifyContent: 'left', display: 'flex', marginBottom: '2%' }}>
+      <div
+        style={{ justifyContent: "left", display: "flex", marginBottom: "2%" }}
+      >
         <button className="toList" onClick={list}>
           목록으로
         </button>
