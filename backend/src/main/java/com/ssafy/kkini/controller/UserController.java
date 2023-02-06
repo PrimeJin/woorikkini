235d26719821f301e5f2128bc91602c2d94a882a
nyh365 <12nyk34@naver.com>
2023-02-06 15:11:36 +0900

    [S08P12A804-138] BE/Fix: edit user delete

--- backend/src/main/java/com/ssafy/kkini/controller/UserController.java
+++ backend/src/main/java/com/ssafy/kkini/controller/UserController.java
@@ -122,7 +122,6 @@ public class UserController {
 
         try {
             if (userService.delete(userid) > 0) {
-                tokenProviderService.deleteRefreshToken(userid);
                 resultMap.put("message", SUCCESS);
                 status = HttpStatus.ACCEPTED;
             } else {
