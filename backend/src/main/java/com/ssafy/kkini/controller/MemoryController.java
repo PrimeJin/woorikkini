22b5e3077b5d4af56d66a19a1569fc846847b643
여민지 <yeomj051@naver.com>
2023-02-09 15:40:07 +0900

    [S08P12A804-262] BE/Fix : get single multipart

--- backend/src/main/java/com/ssafy/kkini/controller/MemoryController.java
+++ backend/src/main/java/com/ssafy/kkini/controller/MemoryController.java
@@ -35,7 +35,7 @@ public class MemoryController {
     @ApiOperation(value = "추억카드 등록", notes = "추억카드 등록")
     @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<Map<String,Object>> createMemory(@Valid @ApiParam(value = "추억 제목,내용, 사진", required = true, example = "0")
-                                                               @RequestPart(value = "memoryImgFiles", required = false) List<MultipartFile> memoryImgFiles,
+                                                               @RequestPart(value = "memoryImgFiles", required = false) MultipartFile memoryImgFiles,
                                                                @RequestPart(value = "newCardData", required = false) MemoryCreateFormDto memoryCreateFormDto){
 
         HttpStatus status = null;
