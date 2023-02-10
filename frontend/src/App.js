fb8930f4e96af0a04b27df6a59b8bb7e7b84809b
YJW <ykm9397@naver.com>
2023-02-10 17:32:27 +0900

    FE/Fix; fix error
    room 가득찬 방 안 보기, adminNotice 링크 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,8 @@ import AdminNoticeDetail from './components/admin/AdminNoticeDetail';
 import AdminNoticeCreate from './components/admin/AdminNoticeCreate';
 import Room from './components/Room';
 import RoomDetail from './components/RoomDetail';
+import Notice from './components/Notice';
+import NoticeDetail from './components/NoticeDetail';
 
 function App() {
   return (
@@ -47,6 +49,8 @@ function App() {
           <Route path="/admin/notice/create" element={<AdminNoticeCreate />} />
           <Route path="/room" element={<Room />} />
           <Route path="/room/:roomId" element={<RoomDetail />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/notice/:noticeId" element={<NoticeDetail />} />
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
