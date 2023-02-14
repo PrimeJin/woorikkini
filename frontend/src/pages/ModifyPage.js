0d8e602ae17e293654818303ee4249981cd4181d
한상원 <wkas1921@naver.com>
2023-02-14 20:53:23 +0900

    [S08P12A804-335] FE/Design: Fix page design
    회원정보 수정, 비밀번호 찾기, 회원탈퇴 페이지 css 수정

--- frontend/src/pages/ModifyPage.js
+++ frontend/src/pages/ModifyPage.js
@@ -137,7 +137,7 @@ function ModifyPage() {
         <CenterLogo />
       </div>
       <form className={styles.modifyform}>
-        <h2>회원정보 수정</h2>
+        <p className={styles.userInfoChange}>회원정보 수정</p>
         <div className={styles.nicknameForm}>
           <input type="text" id={styles.changeNickname} onChange={setUserNickname} placeholder="닉네임"></input>
           <button className={styles.checkButton} onClick={checkNickname}>
@@ -153,7 +153,7 @@ function ModifyPage() {
               placeholder="새 비밀번호"
             />
             {newPassword.length > 0 && (
-              <span className={`message ${isPassword ? 'success' : 'error'}`}>{passwordMsg}</span>
+              <div className={`${isPassword ? styles.messageSuccess : styles.messageError}`}>{passwordMsg}</div>
             )}
             <input
               type="password"
@@ -162,7 +162,9 @@ function ModifyPage() {
               placeholder="새 비밀번호 확인"
             />
             {confirmPasswordMsg.length > 0 && (
-              <span className={`message ${isConfirmPassword ? 'success' : 'error'}`}>{confirmPasswordMsg}</span>
+              <div className={`${isConfirmPassword ? styles.messageSuccess : styles.messageError}`}>
+                {confirmPasswordMsg}
+              </div>
             )}
           </div>
           <button className={styles.modifyButton} onClick={changeUserInfo}>
@@ -170,7 +172,15 @@ function ModifyPage() {
           </button>
         </div>
 
-        <Link to="/user/delete" style={{ color: 'gray', textDecoration: 'none', textAlign: 'left', marginLeft: '12%' }}>
+        <Link
+          to="/user/delete"
+          style={{
+            color: 'gray',
+            textDecoration: 'none',
+            textAlign: 'left',
+            marginLeft: '14%',
+          }}
+        >
           <small>회원 탈퇴</small>
         </Link>
       </form>
